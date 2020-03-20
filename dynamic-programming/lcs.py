115d7ac72e7442b04cb6166bcacf61d5fee99c7f
6f8l <atsuki02jojo@gmail.com>
2020-03-20 11:43:51 +0900

    Fix lcs

--- dynamic-programming/lcs.py
+++ dynamic-programming/lcs.py
@@ -4,7 +4,7 @@ def lcs(a, b):
 
     for i in range(1, x + 1):
         for j in range(1, y + 1):
-            if dp[i - 1] == dp[j - 1]:
+            if a[i - 1] == b[j - 1]:
                 dp[i][j] = dp[i - 1][j - 1] + 1
             else:
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
@@ -15,5 +15,4 @@ if __name__ == "__main__":
     for _ in range(n):
         a = input()
         b = input()
-        print('===========')
         print(lcs(a, b))
